421de27c57b2f84c5ffeb73188ad8b466b19a4e9
vishalmishra5544 <vishalmishra5544@gmail.com>
2025-04-12 20:58:32 +0530

    New Project Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent], // Fixed: RouterOutlet (no space)
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
